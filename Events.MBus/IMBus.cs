1dcb07f02c52e29ad3add868e0650ea80e62aa9c
Mats <mats@greyhouse.se>
2018-10-15 22:28:28 +0200

    Adding more space to file

--- Events.MBus/IMBus.cs
+++ Events.MBus/IMBus.cs
@@ -5,12 +5,15 @@ namespace Events.MBus
     public interface IMBus
     {
         void SubscribeAsync<T>(Action<T> callback);
+        
         void Publish<T>(T t);
 
         void ReceiveAsync<T>(Action<T> callback);
 
         event OnDisconnectedHandler OnDisconnected;
+
         event OnClosedHandler OnClosed;
+        
         void Send<T>(T t);
     }
 }
